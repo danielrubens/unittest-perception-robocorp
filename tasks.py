1648fc795ea323d97e639a7088f367b07b8d9967
danielrubens <contact@danielrubens.com>
2024-04-02 12:22:55 -0300

    call test runner on tasks.py

--- tasks.py
+++ tasks.py
@@ -1,5 +1,7 @@
 from robocorp.tasks import task
-from utilities.pdf.definitiion import HTMLConversor
+from utilities.pdf.definition import HTMLConversor
+
+from __tests__.test_runner import run_test
 
 @task
 def convert_html_to_pdf():
